820b5c16a87b3851eb4e52a249b1a92e2179f256
Guilherme <jogos.coisas@gmail.com>
2021-11-29 15:53:30 -0300

    animação

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin  {
   bool isOpend = false;
   late AnimationController controller;
+  AnimationController? drawerController;
+  AnimationController? drawerListController;
   Animation<double>? rotate;
   Animation<double>? pageLeftMargin;
   Animation<double>? pageTopMargin;
@@ -77,7 +79,11 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
               Positioned(
                 top: 0,
                 left: drawerMargin!.value,
-                child: DrawerWidget(drawerWidth: drawerWidth),
+                child: DrawerWidget(drawerWidth: drawerWidth,
+                callbackOnInit: (dc, dcl) {
+                  drawerController = dc;
+                  drawerListController = dcl;
+                }),
               ),
               //page/body
               Positioned(
@@ -98,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
                 left: arrowMargin!.value,
                 child: GestureDetector(
                   child: Padding(
-                    padding: const EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
                     child: Icon(isOpend ? Icons.arrow_back : Icons.arrow_forward, color: Colors.white),
                   ),
                   onTap: (){
@@ -106,13 +112,17 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
                       isOpend = !isOpend;
                       if(isOpend){
                         controller.forward();
+                        drawerController?.forward();
+                        drawerListController?.forward();
                       }else{
                         controller.reverse();
+                        drawerController?.reverse();
+                        drawerListController?.reverse();
                       }
                     });
                   },
                 ),
-              )
+              ),
             ],
           ),
         )
@@ -123,33 +133,141 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
 
 class DrawerWidget extends StatefulWidget {
   final double drawerWidth;
+  final Function(AnimationController, AnimationController) callbackOnInit;
   const DrawerWidget({
     Key? key,
     required this.drawerWidth,
+    required this.callbackOnInit,
   }) : super(key: key);
 
   @override
   _DrawerWidgetState createState() => _DrawerWidgetState();
 }
 
-class _DrawerWidgetState extends State<DrawerWidget> {
+class _DrawerWidgetState extends State<DrawerWidget> with TickerProviderStateMixin {
+  late AnimationController controller;
+  late AnimationController listController;
 
   @override
   void initState() {
+    controller = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    );
+    listController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    controller.addListener(() {setState(() {});});
+    widget.callbackOnInit(controller, listController);
     super.initState();
   }
 
   @override
   void dispose() {
+    controller.dispose();
+    listController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: MediaQuery.of(context).size.height,
-        width: widget.drawerWidth,
-        color: Colors.blueGrey,
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      height: MediaQuery.of(context).size.height,
+      width: widget.drawerWidth,
+      color: Colors.blueGrey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children:[
+          FadeTransition(
+            opacity: Tween<double>(begin: 0, end: 1).animate(controller),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: const [
+                    Text("Guilherme",
+                      style: TextStyle(fontSize: 18, 
+                        fontWeight: FontWeight.bold, color: Colors.white)),
+                    Text(" "),
+                    Text("Rodrigues",
+                      style: TextStyle(fontSize: 18, color: Colors.grey)),
+                  ],
+                ),
+                const Text("Gerente seila do que",
+                  style: TextStyle(fontSize: 10, color: Colors.grey))
+              ],
+            ),
+          ),
+          const SizedBox(height: 30),
+          if(listController.isCompleted)
+          Column(
+            children: List.generate(5, (index) {
+              return _DrawerItem(
+                icon: const Icon(Icons.ac_unit),
+                label: Text("hhh $index"),
+                millisecondsDelay: 150 * index,
+              );
+            })
+          )
+        ]
+      ),
+    );
+  }
+}
+
+class _DrawerItem extends StatefulWidget {
+  final Widget icon;
+  final Widget label;
+  final int millisecondsDelay;
+  const _DrawerItem({
+    Key? key,
+    required this.icon,
+    required this.label,
+    required this.millisecondsDelay,
+  }) : super(key: key);
+
+  @override
+  State<_DrawerItem> createState() => _DrawerItemState();
+}
+
+class _DrawerItemState extends State<_DrawerItem> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    controller.addListener(() {setState(() {});});
+    Future.delayed(Duration(milliseconds: widget.millisecondsDelay), (){
+      controller.forward();
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: Tween<double>(begin: 0, end: 1).animate(controller),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            widget.icon,
+            const SizedBox(width: 5),
+            widget.label
+          ],
+        ),
+      ),
     );
   }
 }
