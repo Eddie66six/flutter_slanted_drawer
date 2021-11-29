6c681fc74b8671520ea132e90a67e7440bbf4533
Guilherme <jogos.coisas@gmail.com>
2021-11-29 15:09:09 -0300

    melhoria para deixar o drawer na tela inteira

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,8 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
   bool isOpend = false;
   late AnimationController controller;
   Animation<double>? rotate;
-  Animation<double>? pageMargin;
+  Animation<double>? pageLeftMargin;
+  Animation<double>? pageTopMargin;
   Animation<double>? drawerMargin;
   Animation<double>? arrowMargin;
 
@@ -56,15 +57,17 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    var drawerWidth = size.width*0.6;
+    var drawerWidth = size.width;
     if(rotate == null){
       rotate = Tween<double>(begin: 1.0, end: 0.98).animate(controller);
-      pageMargin = Tween<double>(begin: 0.0, end: 1.0).animate(controller);
+      pageLeftMargin = Tween<double>(begin: 0.0, end: 1.0).animate(controller);
+      pageTopMargin = Tween<double>(begin: 0.0, end: 80).animate(controller);
       drawerMargin = Tween<double>(begin: -drawerWidth, end: 0.0).animate(controller);
       arrowMargin = Tween<double>(begin: 0, end: drawerWidth-54).animate(controller);
     }
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.blueGrey,
         body: SizedBox(
           width: size.width,
           height: size.height,
@@ -78,8 +81,8 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
               ),
               //page/body
               Positioned(
-                top: 0,
-                left: (drawerWidth + 30) * pageMargin!.value,
+                top: pageTopMargin!.value,
+                left: (drawerWidth * 0.6 + 30) * pageLeftMargin!.value,
                 child: Transform.rotate(
                   angle: -pi / rotate!.value,
                   child: Container(
@@ -93,9 +96,12 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
               Positioned(
                 top: 0,
                 left: arrowMargin!.value,
-                child: IconButton(
-                  icon: Icon(isOpend ? Icons.arrow_back : Icons.arrow_forward, color: Colors.white),
-                  onPressed: (){
+                child: GestureDetector(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(isOpend ? Icons.arrow_back : Icons.arrow_forward, color: Colors.white),
+                  ),
+                  onTap: (){
                     setState(() {
                       isOpend = !isOpend;
                       if(isOpend){
